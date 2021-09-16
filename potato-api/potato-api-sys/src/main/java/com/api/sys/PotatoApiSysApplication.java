3801b1bf3f157fce348c3e0aa31c9cd6dc2737b7
lizhifu <lizhifuabc@163.com>
2021-09-16 10:00:28 +0800

    新增sys模块

---
+++ potato-api/potato-api-sys/src/main/java/com/api/sys/PotatoApiSysApplication.java
@@ -0,0 +1,16 @@
+package com.api.sys;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 系统模块
+ *
+ * @author lizhifu
+ * @date 2021/9/16
+ */
+public class PotatoApiSysApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PotatoApiSysApplication.class, args);
+    }
+}
