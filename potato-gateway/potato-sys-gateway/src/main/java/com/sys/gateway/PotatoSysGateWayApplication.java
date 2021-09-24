d7ae5d10683919ba5e9e01531aad24a7690ed4e4
lizhifu <lizhifuabc@163.com>
2021-09-24 11:11:40 +0800

    优化描述

--- potato-gateway/potato-sys-gateway/src/main/java/com/sys/gateway/PotatoSysGateWayApplication.java
+++ potato-gateway/potato-sys-gateway/src/main/java/com/sys/gateway/PotatoSysGateWayApplication.java
@@ -2,16 +2,15 @@ package com.sys.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 网关服务
- *
+ * 网关没有数据库服务((exclude = {DataSourceAutoConfiguration.class }))
  * @author lizhifu
  * @date 2021/9/18
  */
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PotatoSysGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PotatoSysGateWayApplication.class, args);
