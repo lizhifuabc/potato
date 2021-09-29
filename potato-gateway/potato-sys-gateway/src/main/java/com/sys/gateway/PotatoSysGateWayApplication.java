8db31d2da324d15e6b1a36a9fa8c13dfb9bc7f6b
lizhifu <lizhifuabc@163.com>
2021-09-29 11:04:07 +0800

    gate way

--- potato-gateway/potato-sys-gateway/src/main/java/com/sys/gateway/PotatoSysGateWayApplication.java
+++ potato-gateway/potato-sys-gateway/src/main/java/com/sys/gateway/PotatoSysGateWayApplication.java
@@ -3,6 +3,7 @@ package com.sys.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 网关服务
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @date 2021/9/18
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableDiscoveryClient
 public class PotatoSysGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(PotatoSysGateWayApplication.class, args);
---
+++ potato-modules/potato-modules-sys/src/main/java/com/potato/sys/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.potato.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * demo
+ *
+ * @author lizhifu
+ * @date 2021/9/29
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+}
