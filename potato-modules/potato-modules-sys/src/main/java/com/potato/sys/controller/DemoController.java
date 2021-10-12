a45b0a250dff84d584a6514396c920de49029b93
lizhifu <lizhifuabc@163.com>
2021-10-12 11:15:02 +0800

    fegin

--- potato-modules/potato-modules-sys/src/main/java/com/potato/sys/controller/DemoController.java
+++ potato-modules/potato-modules-sys/src/main/java/com/potato/sys/controller/DemoController.java
@@ -1,7 +1,11 @@
 package com.potato.sys.controller;
 
+import com.commom.core.base.R;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * demo
@@ -12,5 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-
+    /**
+     * 获取当前用户信息
+     */
+    @GetMapping("/demo")
+    public R<String> demo()
+    {
+        return new R<>().success(UUID.randomUUID().toString());
+    }
 }
